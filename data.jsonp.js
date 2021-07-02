f065f889d3de7ed67c85356d52555ca7f1da2828
RYLS <ryls_subs-sub1@yahoo.co.jp>
2021-07-02 23:33:32 +0900

    Create data.jsonp.js

---
+++ data.jsonp.js
@@ -0,0 +1,13 @@
+// 例
+data({
+	prior_tags: [],
+	default_image: "",
+	items: [
+		{
+			title: "例",
+			image: "path/to/image-file.jpg",
+			tags: ["タグ1", "タグ2"],
+			memos: ["メモ", "めもめも"],
+		},
+	],
+})
